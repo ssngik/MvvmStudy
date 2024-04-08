efa2e081b655a246f18f4890e2f100ad5415762c
김상익 <ksik518@naver.com>
2024-04-08 21:06:25 +0900

    feat : RetrofitService 추가
    싱글톤 패턴으로 RetrofitService 추가

---
+++ app/src/main/java/com/sangik/mvvmstudy/util/RetrofitService.kt
@@ -0,0 +1,13 @@
+package com.sangik.mvvmstudy.util
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitService {
+    private const val BASE_URL = "https://run.mocky.io"
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
